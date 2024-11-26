49b9670bfc311d9b0479ad8fc1c43d6f4e727c04
Louis-Philippe Turmel <turmlo@2020spaces.com>
2024-11-25 23:45:41 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,378 @@
+use color_eyre::owo_colors::OwoColorize;
+use ratatui::crossterm::cursor::SetCursorStyle;
+use ratatui::crossterm::event::{self, Event, KeyCode, KeyEventKind, KeyModifiers};
+use ratatui::crossterm::execute;
+use ratatui::layout::{Constraint, Direction, Layout};
+use ratatui::style::{Color, Style, Styled, Stylize};
+use ratatui::text::{Line, Span, Text};
+use ratatui::widgets::{Block, Borders, Cell, Paragraph, Row, Table, TableState};
+use ratatui::{DefaultTerminal, Frame};
+use std::fmt::Display;
+use std::sync::Arc;
+use std::time::{Duration, Instant};
+use tokio::sync::mpsc;
+use tokio::sync::Mutex;
+use unicode_width::{UnicodeWidthChar, UnicodeWidthStr};
+
+mod ws;
+
+struct App {
+    url: String,
+    input: String,
+    /// Position of the cursor in the input string
+    char_index: usize,
+    result: QueryResult,
+    latency: f32,
+    client: Arc<Mutex<ws::LibSqlClient>>,
+    input_mode: InputMode,
+    table_state: TableState,
+    action_sender: mpsc::UnboundedSender<Action>,
+    res_recv: mpsc::UnboundedReceiver<QueryResult>,
+    latency_recv: mpsc::UnboundedReceiver<f32>,
+}
+
+impl App {
+    pub fn run(mut self, mut terminal: DefaultTerminal) -> color_eyre::Result<()> {
+        self.update_cursor_shape()?;
+
+        let tick_rate = Duration::from_millis(250);
+        let mut last_tick = Instant::now();
+        loop {
+            while let Ok(res) = self.res_recv.try_recv() {
+                self.result = res;
+            }
+            while let Ok(latency) = self.latency_recv.try_recv() {
+                self.latency = latency;
+            }
+            terminal.draw(|f| self.draw(f))?;
+
+            let timeout = tick_rate.saturating_sub(last_tick.elapsed());
+            if event::poll(timeout)? {
+                if let Event::Key(key) = event::read()? {
+                    match self.input_mode {
+                        InputMode::Normal => match key.code {
+                            KeyCode::Char('i') => {
+                                self.input_mode = InputMode::Insert;
+                                self.update_cursor_shape()?;
+                            }
+                            KeyCode::Char('a') => {
+                                self.input_mode = InputMode::Insert;
+                                self.update_cursor_shape()?;
+                                if self.char_index < self.input.len() {
+                                    self.char_index += 1;
+                                }
+                            }
+                            KeyCode::Char('q') => {
+                                return Ok(());
+                            }
+                            KeyCode::Char('0') => {
+                                self.char_index = 0;
+                            }
+                            KeyCode::Char('$') => {
+                                self.char_index = self.input.len() - 1;
+                            }
+                            KeyCode::Char('c') => self.clear_results(),
+                            KeyCode::Char('r') => self.submit_query(),
+                            KeyCode::Left | KeyCode::Char('h') => self.move_cursor_left(),
+                            KeyCode::Right | KeyCode::Char('l') => self.move_cursor_right(),
+                            KeyCode::Char('D') => self.delete_input(),
+                            _ => {}
+                        },
+                        InputMode::Insert if key.kind == KeyEventKind::Press => match key.code {
+                            KeyCode::Char(c) => self.add_char(c),
+                            KeyCode::Left => self.move_cursor_left(),
+                            KeyCode::Right => self.move_cursor_right(),
+                            KeyCode::Backspace => self.delete_char(),
+                            KeyCode::Esc => {
+                                self.input_mode = InputMode::Normal;
+                                self.update_cursor_shape()?;
+                            }
+                            _ => {}
+                        },
+                        InputMode::Insert => {}
+                    }
+                }
+            }
+            if last_tick.elapsed() >= tick_rate {
+                last_tick = Instant::now();
+            }
+        }
+    }
+
+    fn draw(&self, f: &mut Frame) {
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(1)
+            .constraints(
+                [
+                    Constraint::Length(3),  // Top box height
+                    Constraint::Length(10), // Middle box height
+                    Constraint::Min(0),     // Bottom box takes the rest
+                ]
+                .as_ref(),
+            )
+            .split(f.area());
+
+        let top_container = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints([Constraint::Length(32), Constraint::Min(0)].as_ref())
+            .split(chunks[0]);
+
+        // Top box
+        let mode_span = Span::styled(
+            self.input_mode.to_string(),
+            Style::default().bold().bg(Color::Blue).fg(Color::Black),
+        );
+        let latency_span = Span::raw(format!(" | Latency: {}ms", self.latency));
+        let misc_line = Line::from(vec![mode_span, latency_span]);
+        let misc_block =
+            Paragraph::new(misc_line).block(Block::default().borders(Borders::ALL).title("Misc"));
+        f.render_widget(misc_block, top_container[0]);
+
+        let url_block = Paragraph::new(format!("Connected to: {}", self.url))
+            .block(Block::default().borders(Borders::ALL).title("Database URL"));
+        f.render_widget(url_block, top_container[1]);
+
+        // Middle box
+        let query_block = Paragraph::new(self.input.to_string())
+            .block(Block::default().borders(Borders::ALL).title("SQL Query"));
+        f.render_widget(query_block, chunks[1]);
+
+        {
+            let input_area = chunks[1];
+            let input_width = input_area.width - 2;
+            let input_lines = wrap_text(&self.input, input_width);
+            let (cursor_x, cursor_y) = calculate_cursor_position(&input_lines, self.char_index);
+
+            // Set the cursor position
+            f.set_cursor_position((input_area.x + cursor_x + 1, input_area.y + cursor_y + 1));
+        }
+
+        // Bottom box: Results
+        let results_block = match &self.result {
+            QueryResult::None => Paragraph::new("No results")
+                .block(Block::default().borders(Borders::ALL).title("Results")),
+            QueryResult::Table { columns, rows } => {
+                let header_cells = columns
+                    .iter()
+                    .map(|h| Cell::from(Text::from(h.to_string())));
+                let header = Row::new(header_cells).style(
+                    ratatui::style::Style::default()
+                        .fg(ratatui::style::Color::Yellow)
+                        .bg(ratatui::style::Color::Black),
+                );
+
+                let rows = rows.iter().map(|item| {
+                    let cells = item.iter().map(|c| Cell::from(Text::from(c.to_string())));
+                    Row::new(cells)
+                });
+
+                let widths = [Constraint::Length(5), Constraint::Length(5)];
+                let table = Table::new(rows, widths)
+                    .header(header)
+                    .block(Block::default().borders(Borders::ALL).title("Results"))
+                    .widths(
+                        columns
+                            .iter()
+                            .map(|_| Constraint::Min(10))
+                            .collect::<Vec<_>>(),
+                    );
+                f.render_widget(table, chunks[2]);
+                return;
+            }
+            QueryResult::Error(err) => {
+                Paragraph::new(Text::from(err.to_string()).style(Style::default().fg(Color::Red)))
+                    .block(Block::default().borders(Borders::ALL).title("Error"))
+            }
+        };
+        f.render_widget(results_block, chunks[2]);
+    }
+    fn submit_query(&mut self) {
+        if self.input.is_empty() {
+            return;
+        }
+
+        let _ = self.action_sender.send(Action::Query(self.input.clone()));
+    }
+
+    fn update_cursor_shape(&self) -> color_eyre::Result<()> {
+        let cursor = match self.input_mode {
+            InputMode::Normal => SetCursorStyle::SteadyBlock,
+            InputMode::Insert => SetCursorStyle::SteadyBar,
+        };
+        execute!(std::io::stdout(), cursor)?;
+
+        Ok(())
+    }
+
+    fn clear_results(&mut self) {
+        self.result = QueryResult::None;
+    }
+
+    fn delete_input(&mut self) {
+        self.input.clear();
+        self.char_index = 0;
+    }
+
+    fn add_char(&mut self, c: char) {
+        self.input.insert(self.char_index, c);
+        self.char_index += 1;
+    }
+
+    fn delete_char(&mut self) {
+        if self.char_index > 0 {
+            self.input.remove(self.char_index - 1);
+            self.char_index -= 1;
+        }
+    }
+
+    fn move_cursor_left(&mut self) {
+        if self.char_index > 0 {
+            self.char_index -= 1;
+        }
+    }
+    fn move_cursor_right(&mut self) {
+        if self.char_index < self.input.len() - 1 {
+            self.char_index += 1;
+        }
+    }
+}
+
+#[derive(Default, PartialEq, Eq)]
+enum InputMode {
+    #[default]
+    Normal,
+    Insert,
+}
+
+impl Display for InputMode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            InputMode::Normal => write!(f, "Normal"),
+            InputMode::Insert => write!(f, "Insert"),
+        }
+    }
+}
+#[derive(Default)]
+enum QueryResult {
+    #[default]
+    None,
+    Table {
+        columns: Vec<ws::Column>,
+        rows: Vec<Vec<ws::LibSqlValue>>,
+    },
+    Error(String),
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum Action {
+    Query(String),
+}
+
+#[tokio::main]
+async fn main() -> color_eyre::Result<()> {
+    dotenv::dotenv().ok();
+    color_eyre::install()?;
+
+    let token = std::env::var("LIBSQL_TOKEN").expect("LIBSQL_TOKEN not set");
+    let url = "wss://ask-lp-lpturmel.turso.io";
+
+    let mut client = ws::LibSqlClient::connect(url, &token).await?;
+
+    client.open_stream(1).await?;
+
+    let client = Arc::new(Mutex::new(client));
+
+    let (action_tx, mut action_rx) = mpsc::unbounded_channel::<Action>();
+    let (result_tx, result_rx) = mpsc::unbounded_channel::<QueryResult>();
+    let (latency_tx, latency_rx) = mpsc::unbounded_channel::<f32>();
+
+    let app = App {
+        char_index: 0,
+        url: url.to_string(),
+        input: String::new(),
+        result: QueryResult::default(),
+        client: client.clone(),
+        input_mode: InputMode::default(),
+        table_state: TableState::default(),
+        latency: 0.0,
+        action_sender: action_tx,
+        res_recv: result_rx,
+        latency_recv: latency_rx,
+    };
+    let terminal = ratatui::init();
+
+    let client_c = client.clone();
+    tokio::spawn(async move {
+        loop {
+            if let Ok(latency) = client_c.lock().await.send_ping().await {
+                let _ = latency_tx.send(latency);
+            }
+            tokio::time::sleep(Duration::from_millis(1000)).await;
+        }
+    });
+    let client = client.clone();
+    tokio::spawn(async move {
+        while let Some(action) = action_rx.recv().await {
+            match action {
+                Action::Query(query) => {
+                    let mut client = client.lock().await;
+                    let result = client.execute_statement(1, &query).await;
+                    let res = match result {
+                        Ok(res) => QueryResult::Table {
+                            columns: res.cols,
+                            rows: res.rows,
+                        },
+                        Err(err) => QueryResult::Error(err.to_string()),
+                    };
+                    let _ = result_tx.send(res);
+                }
+            }
+        }
+    });
+
+    let app_result = app.run(terminal);
+
+    ratatui::restore();
+
+    app_result
+}
+
+fn wrap_text(text: &str, max_width: u16) -> Vec<String> {
+    let mut lines = Vec::new();
+    let mut current_line = String::new();
+
+    for c in text.chars() {
+        let cw = c.width().unwrap_or(0) as u16;
+        let line_width = UnicodeWidthStr::width(current_line.as_str()) as u16;
+
+        if line_width + cw > max_width {
+            lines.push(current_line);
+            current_line = String::new();
+        }
+        current_line.push(c);
+    }
+
+    lines.push(current_line);
+    lines
+}
+
+fn calculate_cursor_position(lines: &[String], char_index: usize) -> (u16, u16) {
+    let mut chars_remaining = char_index;
+    for (y, line) in lines.iter().enumerate() {
+        let line_length = line.chars().count();
+        if chars_remaining <= line_length {
+            let x = UnicodeWidthStr::width(&line[0..chars_remaining]) as u16;
+            return (x, y as u16);
+        } else {
+            chars_remaining -= line_length;
+        }
+    }
+    let last_line = match lines.last() {
+        Some(line) => line,
+        None => "",
+    };
+    let x = UnicodeWidthStr::width(last_line) as u16;
+    let y = (lines.len() - 1) as u16;
+    (x, y)
+}
---
+++ src/ws.rs
@@ -0,0 +1,363 @@
+use futures::channel::oneshot;
+use futures::stream::SplitSink;
+use futures::{SinkExt, StreamExt};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::fmt::Display;
+use std::sync::Arc;
+use std::time::Instant;
+use tokio::{net::TcpStream, sync::Mutex};
+use tokio_tungstenite::{connect_async_tls_with_config, MaybeTlsStream, WebSocketStream};
+use tungstenite::client::IntoClientRequest;
+use tungstenite::http::header::SEC_WEBSOCKET_PROTOCOL;
+use tungstenite::http::HeaderValue;
+use tungstenite::protocol::WebSocketConfig;
+use tungstenite::Message;
+
+const PING_REQ_ID: i32 = -1;
+const HELLO_REQ_ID: i32 = 1;
+
+pub struct LibSqlClient {
+    writer: SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>,
+    request_id: Arc<Mutex<i32>>,
+    pending: Arc<Mutex<HashMap<i32, oneshot::Sender<ResponseType>>>>,
+}
+
+impl LibSqlClient {
+    pub async fn connect(url: &str, jwt: &str) -> color_eyre::Result<Self> {
+        #![allow(unused_mut)]
+        let mut request = url.into_client_request()?;
+        request.headers_mut().append(
+            SEC_WEBSOCKET_PROTOCOL,
+            HeaderValue::from_str("hrana3").unwrap(),
+        );
+        let config = Some(WebSocketConfig::default());
+        let (ws_stream, _) = connect_async_tls_with_config(request, config, false, None).await?;
+        let (writer, read) = ws_stream.split();
+        let mut client = LibSqlClient {
+            writer,
+            request_id: Arc::new(Mutex::new(1)),
+            pending: Arc::new(Mutex::new(HashMap::new())),
+        };
+        client.spawn_read_loop(read);
+        client.send_hello(jwt).await?;
+        Ok(client)
+    }
+
+    fn spawn_read_loop(
+        &self,
+        mut read: futures::stream::SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>,
+    ) {
+        let pending_responses = self.pending.clone();
+        tokio::spawn(async move {
+            while let Some(msg_result) = read.next().await {
+                match msg_result {
+                    Ok(tungstenite::Message::Text(text)) => {
+                        if let Err(e) = serde_json::from_str::<ResponseMsg>(&text) {
+                            eprintln!("Error parsing response: {}", e);
+                        }
+                        if let Ok(response_msg) = serde_json::from_str::<ResponseMsg>(&text) {
+                            let request_id = response_msg.request_id.unwrap_or(HELLO_REQ_ID);
+                            let response = response_msg.response;
+                            let response_type = response_msg.ty;
+
+                            if let Some(tx) = pending_responses.lock().await.remove(&request_id) {
+                                match response_type.as_str() {
+                                    "hello_ok" => {
+                                        let _ = tx.send(ResponseType::HelloOk);
+                                    }
+                                    "response_error" => {
+                                        if let Some(error) = response_msg.error {
+                                            let _ = tx.send(ResponseType::Error {
+                                                message: error.message,
+                                            });
+                                        }
+                                    }
+                                    _ => {
+                                        if let Some(response) = response {
+                                            let _ = tx.send(response);
+                                        } else {
+                                            println!("{}", text);
+                                        }
+                                    }
+                                }
+                            }
+                        } else {
+                            println!("Received non-response message: {}", text);
+                        }
+                    }
+                    Ok(tungstenite::Message::Close(frame)) => {
+                        println!("Connection closed: {:?}", frame);
+                        break;
+                    }
+                    Ok(other) => match other {
+                        Message::Pong(_) => {
+                            if let Some(tx) = pending_responses.lock().await.remove(&PING_REQ_ID) {
+                                let _ = tx.send(ResponseType::Pong);
+                            }
+                        }
+                        _ => {
+                            println!("Received other message: {:?}", other);
+                        }
+                    },
+                    Err(e) => {
+                        eprintln!("Error in WebSocket stream: {}", e);
+                        break;
+                    }
+                }
+            }
+        });
+    }
+    async fn send_hello(&mut self, jwt: &str) -> color_eyre::Result<()> {
+        let hello_msg = HelloMsg {
+            ty: "hello".to_string(),
+            jwt: jwt.to_string(),
+        };
+
+        let hello_msg_text = serde_json::to_string(&hello_msg)?;
+        self.writer
+            .send(tungstenite::Message::Text(hello_msg_text))
+            .await?;
+
+        let (tx, rx) = oneshot::channel();
+        self.pending.lock().await.insert(HELLO_REQ_ID, tx);
+
+        match rx.await? {
+            ResponseType::HelloOk => Ok(()),
+            _ => Err(color_eyre::eyre::eyre!("Unexpected response for hello")),
+        }
+    }
+
+    pub async fn open_stream(&mut self, stream_id: i32) -> color_eyre::Result<()> {
+        let request_id = self.next_request_id().await;
+
+        let open_stream_req = OpenStreamReq {
+            ty: "request".to_string(),
+            request_id,
+            request: OpenStreamRequest {
+                ty: "open_stream".to_string(),
+                stream_id,
+            },
+        };
+
+        let open_stream_text = serde_json::to_string(&open_stream_req)?;
+        self.writer
+            .send(tungstenite::Message::Text(open_stream_text))
+            .await?;
+
+        let (tx, rx) = oneshot::channel();
+        self.pending.lock().await.insert(request_id, tx);
+
+        match rx.await? {
+            ResponseType::OpenStreamResp {} => Ok(()),
+            _ => Err(color_eyre::eyre::eyre!(
+                "Unexpected response for open_stream"
+            )),
+        }
+    }
+
+    /// Measure latency in milliseconds
+    pub async fn send_ping(&mut self) -> color_eyre::Result<f32> {
+        self.writer.send(Message::Ping(vec![])).await?;
+        let (tx, rx) = oneshot::channel();
+        self.pending.lock().await.insert(PING_REQ_ID, tx);
+        let now = Instant::now();
+        match rx.await? {
+            ResponseType::Pong => Ok(now.elapsed().as_millis() as f32),
+            _ => Err(color_eyre::eyre::eyre!("Unexpected response for ping")),
+        }
+    }
+
+    pub async fn execute_statement(
+        &mut self,
+        stream_id: i32,
+        sql: &str,
+    ) -> color_eyre::Result<StmtResult> {
+        let request_id = self.next_request_id().await;
+
+        let execute_req = ExecuteReq {
+            ty: "request".to_string(),
+            request_id,
+            request: ExecuteRequest {
+                ty: "execute".to_string(),
+                stream_id,
+                stmt: Statement {
+                    sql: sql.to_string(),
+                    args: None,
+                    named_args: None,
+                    want_rows: Some(true),
+                },
+            },
+        };
+
+        let execute_req_text = serde_json::to_string(&execute_req)?;
+        self.writer
+            .send(tungstenite::Message::Text(execute_req_text))
+            .await?;
+
+        // Prepare a channel to receive the response
+        let (tx, rx) = oneshot::channel();
+        self.pending.lock().await.insert(request_id, tx);
+
+        // Wait for the response
+        match rx.await? {
+            ResponseType::ExecuteResp { result } => Ok(result),
+            ResponseType::Error { message } => Err(color_eyre::eyre::eyre!("{}", message)),
+
+            _ => Err(color_eyre::eyre::eyre!(
+                "Unexpected response for execute_statement"
+            )),
+        }
+    }
+
+    async fn next_request_id(&self) -> i32 {
+        let mut counter = self.request_id.lock().await;
+        let request_id = *counter;
+        *counter += 1;
+        request_id
+    }
+}
+
+#[derive(Debug, Serialize)]
+pub struct HelloMsg {
+    #[serde(rename = "type")]
+    pub ty: String,
+    pub jwt: String,
+}
+
+#[derive(Serialize)]
+pub struct OpenStreamReq {
+    #[serde(rename = "type")]
+    pub ty: String,
+    pub request_id: i32,
+    pub request: OpenStreamRequest,
+}
+
+#[derive(Serialize)]
+pub struct OpenStreamRequest {
+    #[serde(rename = "type")]
+    pub ty: String,
+    pub stream_id: i32,
+}
+
+#[derive(Deserialize)]
+pub struct ResponseMsg {
+    #[serde(rename = "type")]
+    pub ty: String,
+    pub request_id: Option<i32>,
+    pub response: Option<ResponseType>,
+    pub error: Option<ErrorType>,
+}
+#[derive(Deserialize)]
+pub struct ErrorType {
+    pub message: String,
+}
+
+#[derive(Deserialize)]
+#[serde(tag = "type")]
+pub enum ResponseType {
+    Pong,
+    Error {
+        message: String,
+    },
+    #[serde(rename = "hello_ok")]
+    HelloOk,
+    #[serde(rename = "open_stream")]
+    OpenStreamResp,
+    #[serde(rename = "execute")]
+    ExecuteResp {
+        result: StmtResult,
+    },
+    // Handle other response types as needed
+}
+
+#[derive(Deserialize)]
+pub struct StmtResult {
+    pub cols: Vec<Column>,
+    pub rows: Vec<Vec<LibSqlValue>>,
+    pub affected_row_count: i64,
+    pub rows_read: i64,
+    pub rows_written: i64,
+    pub query_duration_ms: f64,
+    // Include other fields if necessary
+}
+
+#[derive(Deserialize)]
+pub struct Column {
+    pub name: Option<String>,
+    pub decltype: Option<String>,
+}
+
+impl Display for Column {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.name.as_ref().unwrap_or(&"".to_string()))
+    }
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(tag = "type")]
+pub enum LibSqlValue {
+    #[serde(rename = "null")]
+    Null {},
+    #[serde(rename = "integer")]
+    Integer { value: String },
+    #[serde(rename = "float")]
+    Float { value: f64 },
+    #[serde(rename = "text")]
+    Text { value: String },
+    #[serde(rename = "blob")]
+    Blob { base64: String },
+}
+
+impl Display for LibSqlValue {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            LibSqlValue::Null {} => write!(f, "null"),
+            LibSqlValue::Integer { value } => write!(f, "{}", value),
+            LibSqlValue::Float { value } => write!(f, "{}", value),
+            LibSqlValue::Text { value } => write!(f, "{}", value),
+            LibSqlValue::Blob { base64 } => write!(f, "{}", base64),
+        }
+    }
+}
+
+#[derive(Serialize)]
+pub struct ExecuteReq {
+    #[serde(rename = "type")]
+    pub ty: String,
+    pub request_id: i32,
+    pub request: ExecuteRequest,
+}
+
+#[derive(Serialize)]
+pub struct ExecuteRequest {
+    #[serde(rename = "type")]
+    pub ty: String,
+    pub stream_id: i32,
+    pub stmt: Statement,
+}
+
+#[derive(Serialize)]
+pub struct Statement {
+    pub sql: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub args: Option<Vec<Value>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub named_args: Option<Vec<NamedArg>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub want_rows: Option<bool>,
+}
+
+#[derive(Serialize)]
+pub struct Value {
+    #[serde(rename = "type")]
+    pub ty: String,
+    pub value: Option<String>,
+    pub base64: Option<String>,
+}
+
+#[derive(Serialize)]
+pub struct NamedArg {
+    pub name: String,
+    pub value: Value,
+}
